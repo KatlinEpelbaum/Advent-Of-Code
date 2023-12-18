ea684d18f38334c3da8721acbf7067ee91c7aede
KatlinEpelbaum <145542776+KatlinEpelbaum@users.noreply.github.com>
2023-12-18 13:55:44 +0200

    Day 2

--- day2.py
+++ day2.py
@@ -1,16 +1,24 @@
-f = open("example.txt")
+f = open("input.txt")
 
 lines = f.readlines()
+
+res = 0
 
 for line in lines:
     line_splitted = line.split (":")
     game_id = int(line_splitted[0].split()[1])
+    is_possible = True
 
     for round in line_splitted[1].split(";"):
-        print(round)
+        for color in round.split(","):
+            nr, cn = color.split()
+            nr = int(nr)
 
+            if not (cn == "red" and nr <= 12 or cn == "blue" and nr <= 14 or cn == "green" and nr <= 13):
+                  is_possible = False
 
-    print()
+    print(is_possible, game_id)
+    if is_possible == True:
+        res += game_id
 
-
-# print(lines)
+print("The sum of the game IDs is", res)
